dc1a33251a20a56f568c10e0db3123d772191d97
Royce <83390095+royce0292ng@users.noreply.github.com>
2025-04-21 22:29:49 +1000

    Bug Fix
    page.tsx
    is Blurred resize

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -18,31 +18,33 @@ export default function Home() {
       </div>
       {/* Section 1 TalkHQ */}
       <div className="flex ">
-        <div className="flex bg-white rounded-xl space-x-16 p-8 m-8">
-                <Image isBlurred isZoomed
+        <div className="flex bg-white rounded-xl space-x-16 p-8 m-8 flex-col lg:flex-row ">
+            <div className="flex flex-col justify-center">
+                <Image  isBlurred isZoomed
                   src=" /images/appMock-up.png"
                   alt="TalkHQ mock up"
-                  className="" 
+                  className=""
                 />
-                <div className="  text-black font-bold m-32">
-                  <h1 className={` text-6xl ${quicksand.className} `}>
-                    Talk HQ Mobile App
-                  </h1>  
-                  <p className={` text-xl ${quicksand.className} p-4`}>
-                    TalkHQ, a speech pathology company, aims to address the shortage of speech pathologists by developing a mobile app. The app serves as a platform for parents to connect, share strategies, and access resources while waiting for appointments. It provides articles, blogs, videos, and other materials to empower parents in aiding their child's speech development. 
-                  </p> 
-                  <p className={`  text-gray-500 text-xl ${quicksand.className} px-4 mb-4`}>
-                    The project prioritizes user experience design and iterative development. The app caters to both iOS and Android users, utilizing technologies like React Native, Expo, and AWS. It manages various data types and utilizes modular design for organization, maintainability, and scalability.
-                  </p>
-                  <div className=" space-x-2 p-4">
-                    <Chip>UX/UI</Chip>
-                    <Chip>Java Script</Chip>
-                    <Chip>React Native</Chip>
-                    <Chip>SQL</Chip>
-                    <Chip>AWS</Chip>
-                  </div>
-                </div>
+            </div>
+            <div className="  text-black font-bold m-32">
+              <h1 className={` text-6xl ${quicksand.className} `}>
+                Talk HQ Mobile App
+              </h1>
+              <p className={` text-xl ${quicksand.className} p-4`}>
+                TalkHQ, a speech pathology company, aims to address the shortage of speech pathologists by developing a mobile app. The app serves as a platform for parents to connect, share strategies, and access resources while waiting for appointments. It provides articles, blogs, videos, and other materials to empower parents in aiding their child's speech development.
+              </p>
+              <p className={`  text-gray-500 text-xl ${quicksand.className} px-4 mb-4`}>
+                The project prioritizes user experience design and iterative development. The app caters to both iOS and Android users, utilizing technologies like React Native, Expo, and AWS. It manages various data types and utilizes modular design for organization, maintainability, and scalability.
+              </p>
+              <div className=" space-x-2 p-4">
+                <Chip>UX/UI</Chip>
+                <Chip>Java Script</Chip>
+                <Chip>React Native</Chip>
+                <Chip>SQL</Chip>
+                <Chip>AWS</Chip>
               </div>
+            </div>
+          </div>
       </div>
       
       {/* Know me more Section */}
